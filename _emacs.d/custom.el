7f5f333069999f60033852ef8871172b730982b4
Craig C. Wiegert <ouigui@gmail.com>
2016-06-06 23:35:15 -0400

    Split emacs Customize settings into own file.

---
+++ _emacs.d/custom.el
@@ -0,0 +1,28 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(blink-cursor-mode nil)
+ '(custom-safe-themes
+   (quote
+    ("4f0f2f5ec60a4c6881ba36ffbfef31b2eea1c63aad9fe3a4a0e89452346de278" "9d08af845e2761d07c9312629a31c126fb29b6f570f974d3c283a8146fb9e284" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "7ceb8967b229c1ba102378d3e2c5fef20ec96a41f615b454e0dc0bfa1d326ea6" default)))
+ '(font-latex-fontify-script nil)
+ '(font-latex-fontify-sectioning (quote color))
+ '(font-latex-script-display (quote ((height (+ 1) height (- 1)))))
+ '(font-latex-title-fontify (quote color))
+ '(indicate-buffer-boundaries t)
+ '(indicate-empty-lines t)
+ '(inhibit-startup-screen t)
+ '(safe-local-variable-values (quote ((TeX-master . t)))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :background "#ffffff" :foreground "#000000" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 98 :width normal :foundry "bitstream" :family "Bitstream Vera Sans Mono"))))
+ '(bold-italic ((t (:slant oblique :weight bold))))
+ '(font-lock-string-face ((((class color) (min-colors 88) (background light)) (:foreground "sienna"))))
+ '(italic ((t (:slant oblique))))
+ '(mode-line ((t (:background "SlateBlue" :foreground "wheat1" :box (:line-width 1 :color "SlateBlue" :style released-button)))))
+ '(mode-line-inactive ((default (:inherit mode-line)) (((class color) (min-colors 88) (background light)) (:background "grey90" :foreground "grey20" :box (:line-width 1 :color "grey75") :weight normal)))))
--- _emacs.d/init.el
+++ _emacs.d/init.el
@@ -1,6 +1,6 @@
 ;;; Craig Wiegert's customization file for Emacs
 ;;;
-;;; Time-stamp: "2016-06-06 23:06:05 wigie@lookout.home.net"
+;;; Time-stamp: "2016-06-06 23:34:37 wigie@lookout.home.net"
 
 ;;;
 ;;; Tab completion
@@ -15,13 +15,16 @@
 ;;; Should really remove BBDB; it annoys me now.
 
 ;;; TODO:
-;;; * Enhanced completion
+;;; * Enhanced completion; which one?
 ;;; * Popups?
 ;;; * Setup code modes
 ;;; * Theming
 ;;; * Split inits out of main file
-;;; * Column highlighting
-;;; * Semantic whitespace highlight (in e.g. python-mode)
+;;; * Column highlighting: fill-column-mode vs column-marker
+;;; * Whitespace/indentation highlight (in e.g. python-mode)
+;;; * Updated modeline
+;;; * More useful fringe?
+;;; * Setup variables based on what platform we're on and window system
 
 
 ;; Personal settings path
@@ -207,33 +210,8 @@
 ;; Setup packages
 (require 'init-yasnippet)
 
+;; Keep emacs Customize settings separate
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(when (file-exists-p custom-file)
+  (load custom-file))
 
-;; Customized variables and faces
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(blink-cursor-mode nil)
- '(custom-safe-themes
-   (quote
-    ("9d08af845e2761d07c9312629a31c126fb29b6f570f974d3c283a8146fb9e284" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "7ceb8967b229c1ba102378d3e2c5fef20ec96a41f615b454e0dc0bfa1d326ea6" default)))
- '(font-latex-fontify-script nil)
- '(font-latex-fontify-sectioning (quote color))
- '(font-latex-script-display (quote ((height (+ 1) height (- 1)))))
- '(font-latex-title-fontify (quote color))
- '(indicate-buffer-boundaries t)
- '(indicate-empty-lines t)
- '(inhibit-startup-screen t)
- '(safe-local-variable-values (quote ((TeX-master . t)))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#ffffff" :foreground "#000000" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 98 :width normal :foundry "bitstream" :family "Bitstream Vera Sans Mono"))))
- '(bold-italic ((t (:slant oblique :weight bold))))
- '(font-lock-string-face ((((class color) (min-colors 88) (background light)) (:foreground "sienna"))))
- '(italic ((t (:slant oblique))))
- '(mode-line ((t (:background "SlateBlue" :foreground "wheat1" :box (:line-width 1 :color "SlateBlue" :style released-button)))))
- '(mode-line-inactive ((default (:inherit mode-line)) (((class color) (min-colors 88) (background light)) (:background "grey90" :foreground "grey20" :box (:line-width 1 :color "grey75") :weight normal)))))
